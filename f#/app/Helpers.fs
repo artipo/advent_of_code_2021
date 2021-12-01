d6b405975fde57fbf696a951724c1555945fcecb
federico <fede9421@hotmail.it>
2021-12-01 23:40:20 +0100

    missed_files

--- f#/app/Helpers.fs
+++ f#/app/Helpers.fs
@@ -1,2 +1,36 @@
-﻿module app.Helpers
+﻿[<AutoOpen>]
+module App.Helpers
+
+open System.IO
+
+module File =
+    let readAllLines filePath = File.ReadAllLines(filePath)
+
+module Seq =
+    let toCouples xss =
+        xss
+        |> Seq.map
+            (fun xs ->
+                let a = Seq.item 0 xs
+                let b = Seq.item 1 xs
+                a, b)
+
+    let toTriplets xss =
+        xss
+        |> Seq.map
+            (fun xs ->
+                let a = Seq.item 0 xs
+                let b = Seq.item 1 xs
+                let c = Seq.item 2 xs
+                a, b, c)
+    
+    let mapAsCouples mapper xss =
+        xss
+        |> toCouples
+        |> Seq.map (fun (a, b) -> mapper a b)
+    
+    let mapAsTriplets mapper xss =
+        xss
+        |> toTriplets
+        |> Seq.map (fun (a, b, c) -> mapper a b c)
 
--- f#/app/solutions/Day01.fs
+++ f#/app/solutions/Day01.fs
@@ -1,2 +1,37 @@
-﻿module app.Day01
+﻿module App.Solutions.Day01
 
+open App.Helpers
+
+// utils
+let countChanges change changes =
+    changes
+    |> Seq.filter (fun c -> c = change)
+    |> Seq.length
+
+let rowToMeasurement (r: string) = r |> int
+
+// domain
+type Measurement = int
+
+type MeasurementChange =
+    | Unknown
+    | Smaller
+    | Equal
+    | Larger
+
+let changeInMeasurements m_prev m_next =
+    match m_prev, m_next with
+    | p, n when p > n -> Smaller
+    | p, n when p = n -> Equal
+    | p, n when p < n -> Larger
+    | _ -> Unknown
+
+let mapMeasurementsToChanges measurements =
+    measurements
+    |> Seq.windowed 2
+    |> Seq.mapAsCouples changeInMeasurements
+
+let removeNoiseFromMeasurements (measurements : Measurement list) =
+    measurements
+    |> Seq.windowed 3
+    |> Seq.map Seq.sum
